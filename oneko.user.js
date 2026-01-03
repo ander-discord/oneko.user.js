79a6970ebee8e94fa52ca9fb42ccbbed5938d7cb
fiveappls <fabiosilvatabom1975@gmail.com>
2026-01-02 22:20:51 -0300

    Delete oneko.user.js

--- oneko.user.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name         neko!!!
-// @version      v1
-// @description  a good cat!
-// @author       fiveappls
-// @match        https://*/*
-// @require     https://github.com/ander-discord/oneko.user.js/blob/main/neko.user.js?raw=true
-// ==/UserScript==
